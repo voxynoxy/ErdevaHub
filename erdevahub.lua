d7d3558c78a9338285d5a9f0b4dd5140c412f1db
Erde <unknownwithattitude@proton.me>
2026-08-31 14:35:52 +0700

    Update erdevahub.lua

--- erdevahub.lua
+++ erdevahub.lua
@@ -853,7 +853,7 @@ local function AddInfo(k, v, isLive)
 end
 
 AddInfo("Hub",            "ERDEVA HUB")
-AddInfo("Game",           "Chicken Farm")
+AddInfo("Game",           "Grow a Chicken Fight")
 AddInfo("Plates Grabbed", "0 / 20", true)
 AddInfo("Status",         "Operational")
 
